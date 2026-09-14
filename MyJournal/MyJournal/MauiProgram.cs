730b5d834dacc653dbe33976c32ada133cd3e46d
MaryJoP10 <maryjop10@gmail.com>
2026-09-14 17:14:25 -0500

    Adding MyJournal Carpet

---
+++ MyJournal/MyJournal/MauiProgram.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using MyJournal.Infrastructure.Data;
+using MyJournal.Services.Services;
+using MyJournal.Services.Seed;
+using Microsoft.Maui.Storage;
+using System.IO;
+
+namespace MyJournal
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+            // Register DbContext and application services
+            var dbPath = Path.Combine(FileSystem.AppDataDirectory, "myjournal.db");
+            builder.Services.AddDbContext<MyJournalDbContext>(options => options.UseSqlite($"Data Source={dbPath}"));
+
+            builder.Services.AddScoped<IHabitService, HabitService>();
+            builder.Services.AddScoped<ITaskService, TaskService>();
+            builder.Services.AddScoped<IJournalService, JournalService>();
+            builder.Services.AddSingleton<MyJournal.Services.Theming.IThemeService, MyJournal.Services.Theming.ThemeService>();
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
+#endif
+
+            var app = builder.Build();
+
+            // Seed demo data if appropriate (basic, safe for startup)
+            try
+            {
+                using var scope = app.Services.CreateScope();
+                var db = scope.ServiceProvider.GetRequiredService<MyJournalDbContext>();
+                DemoDataSeeder.SeedAsync(db).GetAwaiter().GetResult();
+            }
+            catch
+            {
+                // seeding is non-blocking; in case of error keep running
+            }
+
+            return app;
+        }
+    }
+}
